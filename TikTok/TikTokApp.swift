7d212c96ca70b2c89d7d13ecc68a55728c470233
NeVajnoKak <156511620+NeVajnoKak@users.noreply.github.com>
2024-07-01 13:10:53 +0500

    Initial Commit

---
+++ TikTok/TikTokApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokApp.swift
+//  TikTok
+//
+//  Created by Erkebulan Massainov on 01.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct TikTokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
