db2beccb3476f0b2c53a5df80c1d0a79f9b26bd5
NeVajnoKak <156511620+NeVajnoKak@users.noreply.github.com>
2024-07-26 03:27:56 +0500

    delete name

--- TikTok/ContentView.swift
+++ TikTok/ContentView.swift
@@ -1,9 +1,4 @@
-//
-//  ContentView.swift
-//  TikTok
-//
-//  Created by Erkebulan Massainov on 01.07.2024.
-//
+
 
 import SwiftUI
 import AVKit
--- TikTok/TikTokApp.swift
+++ TikTok/TikTokApp.swift
@@ -1,9 +1,4 @@
-//
-//  TikTokApp.swift
-//  TikTok
-//
-//  Created by Erkebulan Massainov on 01.07.2024.
-//
+
 
 import SwiftUI
 
